009a2280dfae495a6a04a6bd0bdab51cea3932dc
Tanya Chetna Vaish <63603952+TanyaChetnaVaish@users.noreply.github.com>
2020-10-02 15:10:49 +0530

    Create InputScanner.java

---
+++ InputScanner.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class InputSc 
+{ 
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Enter the number:");
+    int n=sc.nextInt();
+    System.out.println("The number is:"+n);
+   }
+ }
+    
