381efea0084c7b1fbd54f10d05d88bb83828e795
meraj-11ahmed <72243121+meraj-11ahmed@users.noreply.github.com>
2020-10-02 14:10:43 +0530

    Create Operators.java

---
+++ Operators.java
@@ -0,0 +1,15 @@
+class Operators
+{
+  public static void main(String[] args)
+  {
+    int a=10,b=2,c;
+    c=a+b;
+    System.out.println(" +  operator "+c);
+    c=a-b;
+    System.out.println(" +  operator "+c);
+    c=a*b;
+    System.out.println(" +  operator "+c);
+    c=a/b;
+    System.out.println(" +  operator "+c);
+   }
+ }
